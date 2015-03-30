033e177acb051a59d2c3690edf429986e1028e16
Fornoth <fornoth01@gmail.com>
2015-03-30 00:04:35 +0000

    Add proper parameters for SpPlaybackEnableShuffle and SpPlaybackEnableRepeat

--- common/spotify.h
+++ common/spotify.h
@@ -168,8 +168,8 @@ int SpPlaybackIsPlaying(void);
 int SpPlaybackIsShuffled(void);
 int SpPlaybackIsRepeated(void);
 int SpPlaybackIsActiveDevice(void);
-sp_err_t SpPlaybackEnableShuffle(void);
-sp_err_t SpPlaybackEnableRepeat(void);
+sp_err_t SpPlaybackEnableShuffle(bool enable);
+sp_err_t SpPlaybackEnableRepeat(bool enable);
 sp_err_t SpPlaybackSetBitrate(sp_bitrate_t bitrate);
 
 sp_err_t SpConnectionLoginPassword(const char *login, const char *password);
